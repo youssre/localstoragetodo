ceff65ca6c0c88d5a645ab8b9eb9f4179f6b84d9
youssre <youssre092@gmail.com>
2022-01-29 14:57:14 +0200

    first commit by yousri

---
+++ lStorage.js
@@ -0,0 +1,71 @@
+/*global console */
+let allspans=document.querySelectorAll('.buttons span');
+let results=document.querySelector(".results>span");
+let theinput=document.getElementById("name");
+allspans.forEach(span=>{
+    span.addEventListener("click",(e)=>{
+      if(e.target.classList.contains("check")){
+        checkItem();
+      } 
+      if(e.target.classList.contains("add")){
+        addItem();
+      } 
+      if(e.target.classList.contains("delete")){
+        deleteItem();
+      } 
+      if(e.target.classList.contains("show")){
+        showItem();
+      } 
+
+    })
+
+});
+
+function showMessage(){
+    results.innerHTML = 'the input cant be empty';
+}
+function checkItem(){
+    if(theinput.value !==''){
+      if(localStorage.getItem(theinput.value)){
+        results.innerHTML=`Found Local Item Called <span>${theinput.value}</span>`;
+      } else{
+        results.innerHTML=`No Local Storage Item With The Name <span>${theinput.value}</span>`;
+      }
+    } else{
+      showMessage();
+    }
+}
+
+
+function addItem(){
+  if(theinput.value !==''){
+    localStorage.setItem(theinput.value,"test");
+    results.innerHTML=`Local Storage Item <span>${theinput.value}</span>added`;
+    theinput.value='';
+  } else{
+    showMessage();
+  }
+}
+function deleteItem(){
+  if(theinput.value !==''){
+    if(localStorage.getItem(theinput.value)){
+      localStorage.removeItem(theinput.value);
+      results.innerHTML=`Local Storsge Item Called <span>${theinput.value}</span>Deleted`;
+      theinput.value='';
+    } else{
+      results.innerHTML=`No Local Storage Item With The Name <span>${theinput.value}</span>`;
+    }
+  } else{
+    showMessage();
+  }
+}
+function showItem(){ 
+  if(localStorage.length){
+    //console.log(`Found elements .${localStorage.length}`);
+    for(let[key,value] of Object.entries(localStorage)){
+      results.innerHTML+=`<span>${key}=>${value}</span>`
+    }
+  } else{
+    results.innerHTML=`No Items in Local Storage`;
+  }
+}
